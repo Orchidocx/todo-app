7ad5a8005484fda1bf6a16222c217e271864a500
Jacky Li <ilikemacaronii@gmail.com>
2020-09-22 15:59:17 -0700

    add todo component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
 import "./App.css";
+import Todo from "./Todo";
 
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
 
-  const addATodo = () => {
+  const addATodo = (event) => {
+    event.preventDefault();
     setTodos([...todos, inputText]);
     setInputText("");
   };
   return (
     <div className="app">
       <h1>Todo App</h1>
-      <input
-        value={inputText}
-        onChange={(event) => setInputText(event.target.value)}
-        type="text"
-      />
-      <button onClick={addATodo}>ADD A TODO</button>
+      <form>
+        <input
+          value={inputText}
+          onChange={(event) => setInputText(event.target.value)}
+          type="text"
+        />
+        <button disabled={!inputText} type="submit" onClick={addATodo}>
+          ADD A TODO
+        </button>
+      </form>
+      {todos.map((todo) => (
+        <Todo text={todo} />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Todo({ text }) {
+  return <div>{text}</div>;
+}
+
+export default Todo;
