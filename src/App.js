7cf6ca3cf235f20ab1167aac2313f85bc57b360e
Jacky Li <ilikemacaronii@gmail.com>
2020-09-22 13:46:53 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addATodo = () => {
+    setTodos([...todos, inputText]);
+    setInputText("");
+  };
   return (
     <div className="app">
       <h1>Todo App</h1>
-      <input type="text"></input>
-      <button>ADD A TODO</button>
+      <input
+        value={inputText}
+        onChange={(event) => setInputText(event.target.value)}
+        type="text"
+      />
+      <button onClick={addATodo}>ADD A TODO</button>
     </div>
   );
 }
