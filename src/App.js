39f514900fc1529ab595141864e61662d835551b
Jacky Li <ilikemacaronii@gmail.com>
2020-09-22 13:25:48 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <h1>Todo App</h1>
+      <input type="text"></input>
+      <button>ADD A TODO</button>
     </div>
   );
 }
